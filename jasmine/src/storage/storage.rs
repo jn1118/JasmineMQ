9612668c66cec9f8b72611e241a12ecfac32d122
daixi98 <x3dai@ucsd.edu>
2022-05-21 16:58:21 -0700

    redis

--- jasmine/src/storage/storage.rs
+++ jasmine/src/storage/storage.rs
@@ -1,4 +1,6 @@
 use util::result::JasmineResult;
+extern crate redis;
+
 
 pub struct Storage {
 
@@ -8,6 +10,8 @@ impl Storage {
 
     /// Store a vector of u8, return the key
     pub fn set(bytes: Vec<u8>) -> JasmineResult<String> {
+        let client = redis::Client::open("redis://127.0.0.1/")?;
+        let mut con = client.get_connection()?;
         let key = "key".to_string();
         return Ok(key);
     }
