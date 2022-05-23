46d62f22c7a3f27e28ddf8accff87a3777986853
jn1118 <jix209@ucsd.edu>
2022-05-22 21:06:26 -0700

    yo

--- jasmine/src/client/client.rs
+++ jasmine/src/client/client.rs
@@ -1,58 +1,66 @@
 use super::{publisher::JasminePublisher, subscriber::JasmineSubscriber};
-use crate::broker::server::JasmineBroker;
 use async_trait::async_trait;
 use std::collections::{HashMap, HashSet};
+use util::rpc::broker::jasmine_broker_server::JasmineBrokerServer;
+use util::rpc::client::jasmine_client_client::JasmineClientClient;
 // use tonic::Status;
 use util::{
     result::{JasmineError, JasmineResult},
+    // rpc::broker::jasmine_broker_server::JasmineBroker,
     transaction::JasmineMessage,
 };
 
 ///A trait representing a JasmineClient interface. The trait bounds for JasminePublisher and JasmineSubscriber respectively.
 pub trait JasmineClient: JasminePublisher + JasmineSubscriber + Send + Sync {
     ///A function creates and returns the object
-    // fn new(&self, client_id: u64, broker: Box<dyn JasmineBroker>) -> JasmineResult<Box<Self>>;
+    fn new(&self, broker: Vec<String>) -> JasmineResult<Box<Self>>;
     ///A function connects the client
-    fn connect(&mut self, user: &str) -> JasmineResult<()>;
+    fn connect(&self) -> JasmineResult<()>;
     ///A function disconnets the client
-    fn disconnect(&mut self, user: &str) -> JasmineResult<()>;
+    fn disconnect(&self) -> JasmineResult<()>;
     fn on_message(&self) -> JasmineMessage;
 }
 
 /// This struct includes features and functionalities of a frontend mqtt like client
 pub struct Client {
     /// Unique client id
-    pub client_id: u64,
-    pub client_map: HashMap<String, u64>,
-    pub broker: Box<dyn JasmineBroker>,
+    // pub client_id: u64,
+    // pub client_map: HashMap<String, u64>,
+    pub broker_addr: Vec<String>,
 }
 
 impl JasmineClient for Client {
-    // fn new(&self, clientid: u64, broker: Box<dyn JasmineBroker>) -> JasmineResult<Box<Self>> {
-    //     return Ok(Box::new(Client {
-    //         client_id: clientid,
-    //         broker: broker,
-    //     }));
-    // }
-
-    fn connect(&mut self, user: &str) -> JasmineResult<()> {
-        let client_exist = self.client_map.get(user);
-        let mut id = 0;
-        match client_exist {
-            Some(value) => {
-                id = *value;
-            }
-            None => {
-                self.client_map.insert(user.to_string(), self.client_id);
-                id = self.client_id;
-                self.client_id = self.client_id + 1;
-            }
-        }
-        // do something using id
-        return Ok(());
+    fn new(&self, broker: Vec<String>) -> JasmineResult<Box<Self>> {
+        return Ok(Box::new(Client {
+            broker_addr: broker,
+        }));
     }
 
-    fn disconnect(&mut self, user: &str) -> JasmineResult<()> {
+    fn connect(&self) -> JasmineResult<()> {
+        let mut broker =
+            JasmineBrokerServer::connect(format!("http://{}", &self.broker_addr[0])).await;
+
+        // let broker = connect(self.broker_addr[0]);
+        return Ok(());
+    }
+    // fn connect(&mut self, user: &str) -> JasmineResult<()> {
+    //     let client_exist = self.client_map.get(user);
+    //     let mut id = 0;
+    //     match client_exist {
+    //         Some(value) => {
+    //             id = *value;
+    //         }
+    //         None => {
+    //             self.client_map.insert(user.to_string(), self.client_id);
+    //             id = self.client_id;
+    //             self.client_id = self.client_id + 1;
+    //         }
+    //     }
+    //     // do something using id
+    //     return Ok(());
+    // }
+
+    fn disconnect(&self) -> JasmineResult<()> {
         todo!()
     }
 
@@ -63,15 +71,17 @@ impl JasmineClient for Client {
 #[async_trait]
 impl JasminePublisher for Client {
     async fn publish(&self, topic: String, message: JasmineMessage) -> JasmineResult<()> {
-        let a = self.broker.on_pub_message(&topic, &message).await;
-        match a {
-            Ok(_) => {
-                return Ok(());
-            }
-            Err(e) => {
-                return Err(e);
-            }
-        }
+        return Ok(());
+        // self.connect(user)
+        // let a = self.publish(&topic, &message).await;
+        // match a {
+        //     Ok(_) => {
+        //         return Ok(());
+        //     }
+        //     Err(e) => {
+        //         return Err(e);
+        //     }
+        // }
     }
 }
 
--- jasmine/src/lab.rs
+++ jasmine/src/lab.rs
@@ -1,17 +1,17 @@
-use crate::broker::server::JasmineBroker;
+// use crate::broker::server::JasmineBroker;
 use crate::client::client::Client;
 use std::collections::{HashMap, HashSet};
 // use crate::client::client::JasmineBroker;
 use crate::client::client::JasmineClient;
 use util::result::JasmineResult;
 
-#[allow(unused_variables)]
-pub async fn new_front_client(
-    broker: Box<dyn JasmineBroker>,
-) -> JasmineResult<Box<dyn JasmineClient + Send + Sync>> {
-    Ok(Box::new(Client {
-        client_id: 0,
-        client_map: HashMap::new(),
-        broker: broker,
-    }))
-}
+// #[allow(unused_variables)]
+// pub async fn new_front_client(
+//     broker_addr: Vec<String>,
+// ) -> JasmineResult<Box<dyn JasmineClient + Send + Sync>> {
+//     Ok(Box::new(Client {
+//         // client_id: 0,
+//         // client_map: HashMap::new(),
+//         broker_addr: broker_addr,
+//     }))
+// }
